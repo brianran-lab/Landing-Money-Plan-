5cc16773413dbb7d734b554fde5aec112cbb58ec
brianran-lab <brianran@msn.com>
2026-08-06 18:28:17 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Website from "./pages/Website.jsx";
 import Plan from "./pages/Plan.jsx";
+import AuthGate from "./AuthGate";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Website />} />
-        <Route path="/plan" element={<Plan />} />
+                        <Route path="/plan" element={<AuthGate><Plan /></AuthGate>} />
       </Routes>
     </BrowserRouter>
   );
